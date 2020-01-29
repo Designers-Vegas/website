2543add1f87b51263de6e08cdfed8d2ea9bc8d5a
Jose Figueroa <Josefigueroalv@protonmail.com>
2020-01-29 00:12:24 -0800

    first staging commit

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ const Home = () => (
       <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
+      <br/>
+      <p>Futuer Home of Designers.Vegas!</p>
 
       <div className="row">
         <a href="https://nextjs.org/docs" className="card">
