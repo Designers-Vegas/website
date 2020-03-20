90a4a12bf135364fdf461820ad1964e275f9171d
Jose Figueroa <Josefigueroalv@protonmail.com>
2020-03-19 22:57:12 -0700

    removed description block

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ const Home = () => (
 
     <div className="hero">
       <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-      <br />
       <p className="description">Future Home of Designers.Vegas!</p>
 
       <div className="row">
