154d242de55ce18e93cae14cb6935fd85caf1ba3
Jose Figueroa <Josefigueroalv@protonmail.com>
2020-04-06 14:34:03 -0600

    created about page

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Layout from '../components/myLayout';
+
+const About = () => (
+  <Layout Page={'About'}>
+    <h1>About Us</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <h2>Organizers</h2>
+    <h2>Founder</h2>
+  </Layout>
+);
+
+export default About;
