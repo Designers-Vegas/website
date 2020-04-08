db0734237d5e04d7d805fd50dce767d035e2fd4c
Jose Figueroa <Josefigueroalv@protonmail.com>
2020-04-08 13:34:00 -0600

    no longer using a CSS global baseline

--- components/myLayout.js
+++ components/myLayout.js
@@ -1,11 +1,9 @@
 import Head from 'next/head';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 const Layout = ({ Page, children }) => (
   <div>
-    <CssBaseline />
     <Head>
       <meta
         name="viewport"
@@ -13,14 +11,36 @@ const Layout = ({ Page, children }) => (
       ></meta>
       <title>{Page}</title>
       <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-      />
     </Head>
     <Nav />
     {children}
     <Footer />
+    <style jsx>{`
+      *,
+      *::before,
+      *::after {
+        box-sizing: inherit;
+      }
+      user agent stylesheet h1 {
+        display: block;
+        font-size: 2em;
+        margin-block-start: 0.67em;
+        margin-block-end: 0.67em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        font-weight: bold;
+      }
+      body {
+        color: rgba(0, 0, 0, 0.87);
+        margin: 0;
+        font-size: 0.875rem;
+        font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+        font-weight: 400;
+        line-height: 1.43;
+        letter-spacing: 0.01071em;
+        background-color: #fafafa;
+      }
+    `}</style>
   </div>
 );
 
